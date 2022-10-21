b23b6867b15eb68239ac8f01323e445447a30a72
adrianrezha29 <adrianrezha29@gmail.com>
2022-10-21 16:58:31 +0700

    Add project files.

---
+++ QuickSort_20210140060/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort_20210140060
+{
+    class Program
+    {
+        //array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; //number of comparasion
+        private int mov_count = 0; //number of data movements
+
+        //Number of elements in array
+        {
+        }
+    }
+}
